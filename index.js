2879df11392571ddf9aae1ae2cee1ba36080c1e0
shanemmay <shanemarvinmay@hotmail.com>
2018-12-26 22:22:21 -0600

    basic backend

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => 
+{
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>App Running!</h1>');
+});
+
+app.listen(port, () => { console.log(`App running on port ${port}`)});
